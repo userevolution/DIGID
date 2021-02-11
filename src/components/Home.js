3305291b67880bdb40ac16a37e975ac1f8ae15d5
You Song Hou <yousonghou@yahoo.com>
2021-02-11 17:53:15 -0500

    change the images on the home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,26 +4,33 @@ import { Link } from 'react-router-dom';
 function Home(){
 	return(
 		<div className="bg-primary-color">
-			<div className="container min-height py-5">
+			<div className="container min-height">
 				<div className="row">
 					 <div className="col-12 col-md-6">
-						<img className="hero-img" src="images/phone.png" alt="Demo" />
+						<img className="hero-img" src="images/phone1.png" alt="Demo" />
 					 </div>
 
-					 <div className="col-12 col-md-6 mt-5">
-						<h2 className="text-secondary-color mt-5 text-center title">
+					 <div className="col-12 col-md-6">
+						<h2 className="text-secondary-color text-center title mb-3">
 							Share Files Privately
 						</h2>
 						<Link className="btn bg-secondary-color btn-lg d-block mx-auto w-75" to="login">
 							Sign In
 						</Link>
-						<p className="text-secondary-color mt-5 text-center h3">
+					 </div>
+
+					 <div className="col-12 col-md-6">
+						<img className="hero-img" src="images/phone2.png" alt="Demo" />
+					 </div>
+
+					 <div className="col-12 col-md-6">
+						<p className="text-secondary-color mt-5 text-center h2 mt-10">
 							Secure Identitiy
 						</p>
-						<p className="text-secondary-color text-center h3">
+						<p className="text-secondary-color text-center h2">
 							Upload and Share Files
 						</p>
-						<p className="text-secondary-color text-center h3 mt-5">
+						<p className="text-secondary-color text-center h2 mt-5">
 							DIGID runs on Ceramic Network
 						</p>
 					 </div>
