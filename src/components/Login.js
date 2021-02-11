ad8d5cf1b0d26a241a28188d637a50db24138016
You Song Hou <yousonghou@yahoo.com>
2021-02-10 23:37:31 -0500

    add bootstrap styles to the login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,8 +2,16 @@ import React from 'react';
 
 function Login({authenticate, idxId, name, description}) {
   return (
-    <div className="container">
-        <button onClick={authenticate}>Login</button>
+    <div className="container d-flex align-items-center justify-content-center" style={{ minHeight: "80vh" }}>
+        <div className="card w-100" style={{ maxWidth: '400px'}}>
+          <div className="card-body mx-auto">
+            <img className="d-block mb-5 logo" style={{ width: '200px'}} src="images/iconlogo.png" alt="Demo" />
+            <button className="btn bg-secondary-color btn-block" onClick={authenticate}>
+              Login
+            </button>
+          </div>
+        </div>
+        
         <p>{idxId}</p>
         <p>{name}</p>
         <p>{description}</p>
